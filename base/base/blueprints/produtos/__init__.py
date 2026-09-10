9d68f4986f3cd6c98e8db93066d87de48237264a
brenda354 <w.brenda@escolar.ifrn.edu.br>
2026-09-10 17:34:29 -0300

    Delete base directory

--- base/base/blueprints/produtos/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-produtos_bp = Blueprint("produtos", __name__, template_folder="templates")
-
-from blueprints.produtos import routes
--- base/base/blueprints/produtos/routes.py
+++
@@ -1,9 +0,0 @@
-# Este arquivo ainda não é usado pela aplicação.
-from app import app
-from flask import render_template
-import models
-
-
-@app.route("/produtos")
-def listar_produtos():
-    return render_template("produtos/index.html", produtos=models.produtos)
